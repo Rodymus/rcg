5b12892e35c48ce7fb413e623ea85e61c07692a7
Rodymus <robertogonzalez.ar@gmail.com>
2022-07-29 20:50:00 -0300

    trabajando en la experiencia, por ahora falla y no carga las personas

--- src/main/java/com/portafoliorcg/rcg/Controller/PersonaController.java
+++ src/main/java/com/portafoliorcg/rcg/Controller/PersonaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("/personas/traer")
+    @GetMapping("personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
@@ -29,38 +29,34 @@ public class PersonaController {
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
-       ipersonaService.savePersona(persona);
-       return "La persona fue creada correctamente";
+        ipersonaService.savePersona(persona);
+        return "La persona fue creada correctamente";
     }
     
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/personas/borrar/(id)")
+    @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
     
-    //URL:PUERTO/personas/editar/nombre&apellido&img
     @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/personas/editar/(id)")
+    @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
-            @RequestParam("nombre") String nuevoNombre,
-            @RequestParam("apellido") String nuevoApellido,
-            @RequestParam("sobremi") String nuevoSobremi,
-            @RequestParam("img") String nuevoImg){
-        
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido,
+                               @RequestParam("img") String nuevoImg){
         Persona persona = ipersonaService.findPersona(id);
         
         persona.setNombre(nuevoNombre);
         persona.setApellido(nuevoApellido);
-        persona.setSobremi(nuevoSobremi);
         persona.setImg(nuevoImg);
         
         ipersonaService.savePersona(persona);
         return persona;
     }
     
-    @GetMapping("/personas/traer/perfil")
+    @GetMapping("personas/traer/perfil")
     public Persona findPersona(){
         return ipersonaService.findPersona((long)1);
     }
