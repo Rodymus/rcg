a09c7b7b4fd81295782e8b08141c8f247d4749ec
Rodymus <61990609+Rodymus@users.noreply.github.com>
2023-02-14 20:06:38 -0300

    Update PersonaController.java

--- src/main/java/com/portafoliorcg/rcg/Controller/PersonaController.java
+++ src/main/java/com/portafoliorcg/rcg/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://deploy-portfolio-aab53.web.app/","http://localhost:4200})
+@CrossOrigin(origins = {"https://deploy-portfolio-aab53.web.app/","http://localhost:4200"})
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
